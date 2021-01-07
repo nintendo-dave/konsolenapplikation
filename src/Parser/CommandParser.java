da9126c8b6ada6a4695979dd76bb579011f79d0d
kol.d330 <kol.d330@gmail.com>
2021-01-07 20:38:37 +0100

    Compiler Fehler verbessert, in allen Klassen

--- src/Parser/CommandParser.java
+++ src/Parser/CommandParser.java
@@ -1,20 +1,19 @@
 package Parser;
 
 import Command.Command;
-import Writer.IOutputWriter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommandParser {
     public static List<String> parseCommandParameters(String parameters){
-       return new ArrayList<>();
+       return new ArrayList<String>();
     }
 
     public static Command parseCommand(String userInput){
         return new Command() {
             @Override
-            protected void execute(IOutputWriter outputWriter) {
+            protected void execute() {
 
             }
         };
