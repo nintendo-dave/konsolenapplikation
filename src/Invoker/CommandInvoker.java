01ef7cf38d12a128f2bfbfec24c966777fdb75f3
kol.d330 <kol.d330@gmail.com>
2021-01-28 13:29:50 +0100

    CommandInvoker code erstellt um CommandParser einzubinden

--- src/Invoker/CommandInvoker.java
+++ src/Invoker/CommandInvoker.java
@@ -1,20 +1,36 @@
 package Invoker;
 
 import Command.Command;
+import Factory.CommandFactory;
+import Parser.CommandParser;
 import Writer.ConsoleOutputWriter;
 import Writer.IOutputWriter;
 
 import java.util.Dictionary;
+import java.util.List;
 
 public class CommandInvoker {
     Dictionary<String, Command> commands;
     ConsoleOutputWriter outputWriter;
     public CommandInvoker(IOutputWriter outputWriter) {
         this.outputWriter = (ConsoleOutputWriter) outputWriter;
+        commands = CommandFactory.getCommands();
     }
 
     public void executeCommand(String userInput){
-
+        String commandName = userInput.split(" ")[0];
+        String commandParameters = "";
+        for(int i = 1; i < userInput.split(" ").length; i++){
+            if(i != userInput.split(" ").length - 1) {
+                commandParameters += userInput.split(" ")[i] + " ";
+            }
+            else{
+                commandParameters += userInput.split(" ")[i];
+            }
+        }
+        Command command = CommandParser.parseCommand(commandName);
+        CommandFactory.getCommand(command);
+        List<String> parameters = CommandParser.parseCommandParameters(commandParameters);
     }
 
     public void addCommand(){
