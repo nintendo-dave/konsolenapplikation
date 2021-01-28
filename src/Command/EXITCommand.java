c2760dc79f0eafe979f08ca037bdccd8d598c1a7
Silvan Lendi <silvan.lendi@gmx.ch>
2021-01-28 13:12:46 +0100

    exit command rename refactored

--- src/Command/EXITCommand.java
+++ src/Command/EXITCommand.java
@@ -4,7 +4,7 @@ import Writer.IOutputWriter;
 
 import java.util.List;
 
-public class ExitCommand extends Command {
+public class EXITCommand extends Command {
     @Override
     public void execute(IOutputWriter outputWriter) {
         System.exit(0);
--- src/Command/VERCommand.java
+++ src/Command/VERCommand.java
@@ -3,9 +3,11 @@ package Command;
 import Writer.IOutputWriter;
 
 public class VERCommand extends Command {
+    //Variables
+    String version = "Alpha 0.1";
+
     @Override
     protected void execute(IOutputWriter outputWriter) {
-
+        outputWriter.printLine(version);
     }
-
 }
