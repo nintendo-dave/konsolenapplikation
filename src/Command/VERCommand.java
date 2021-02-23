683f89b580489f982ed12974233331ac2264cb8b
remosteiner <remo.steiner@bwz-rappi.ch>
2021-02-23 11:55:01 +0100

    DELCommandTest

--- src/Command/VERCommand.java
+++ src/Command/VERCommand.java
@@ -5,7 +5,7 @@ import Writer.IOutputWriter;
 
 public class VERCommand extends Command {
     //Variables
-    String version = "Alpha 1.0";
+    String version = "Alpha 0.1";
 
     @Override
     public void execute(IOutputWriter outputWriter, Drive drive) {
