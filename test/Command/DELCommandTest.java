73b03572f18113935b58a5e64ed1a54c1de79b6d
Silvan Lendi <silvan.lendi@gmx.ch>
2021-03-02 10:53:15 +0100

    delcommand test sorta done

--- test/Command/DELCommandTest.java
+++ test/Command/DELCommandTest.java
@@ -1,9 +1,15 @@
 package Command;
 
+import Console.Console;
+import Factory.CommandFactory;
 import Filesystem.Directory;
 import Filesystem.Drive;
 import TestWriter.TestOutputWriter;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DELCommandTest {
 
@@ -12,22 +18,35 @@ public class DELCommandTest {
 
         //Arrange
         //--- Variables
-        TestOutputWriter writer;
+        TestOutputWriter testOutputWriter;
         Drive drive;
         Directory rootDir;
         Directory dir;
+        List<String> params = new ArrayList<>();
 
-        writer = new TestOutputWriter();
+        testOutputWriter = new TestOutputWriter();
         drive = new Drive("TestDrive", "C");
         rootDir = new Directory("sys", "C:\\");
         dir = new Directory("cec", "C:\\sys");
-        dir.setPath(rootDir.getPath());
 
+        params.add("sys");
 
+        drive.setRootDirectory(rootDir);
+
+        rootDir.getFileSystemLists().add(dir);
+
+        drive.getCurrentDirectory().getFileSystemLists().add(rootDir);
+
+        drive.setCurrentDirectory(rootDir);
+
+        Command cmd = CommandFactory.getCommand("del");
+        cmd.setParameters(params);
+        cmd.execute(testOutputWriter, drive);
         //Act
 
 
         //Assert
+        Assert.assertEquals("Das File mit dem namen sys wurde gelöscht", testOutputWriter.getOutput());
         //assertEquals("Das File mit dem namen "+fileName+ " wurde gelöscht", TestOutputWriter.getOutput());
     }
 }
