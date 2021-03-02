235b71de7b6f76588db2a2ab42433231729e3466
Zuccerberg <davi.bosch@outlook.com>
2021-03-02 11:13:22 +0100

    Demofähige Version

--- src/Command/CDCommand.java
+++ src/Command/CDCommand.java
@@ -6,12 +6,9 @@ public class CDCommand extends Command {
     @Override
     public void execute(IOutputWriter outputWriter, Drive drive) {
         if(parameters.get(0).equals("\\") || parameters.get(0).equals("/")){
-            System.out.println("Something1");
             drive.getCurrentDirectory().setPath("C:");
-            System.out.println(drive.getCurrentDirectory().getPath());
         }
         else if(parameters.get(0).equals("..")) {
-            System.out.println("Something2");
             boolean isRootDirectory = false;
             String[] array = drive.getCurrentDirectory().getPath().split("/");
             String path = "";
@@ -27,10 +24,8 @@ public class CDCommand extends Command {
                 }
                 drive.getCurrentDirectory().setPath(path);
             }
-            System.out.println(drive.getCurrentDirectory().getPath());
         }
         else if(parameters.get(0).startsWith("..")){
-            System.out.println("Something3");
             boolean allAreSame = false;
             String[] array = parameters.get(0).split("/");
             for(int i = 0; i < array.length; i++){
@@ -45,7 +40,6 @@ public class CDCommand extends Command {
                 }
             }
             if(allAreSame) {
-                System.out.println("Something4");
                 String[] array1 = drive.getCurrentDirectory().getPath().split("/");
                 String path = "";
                 for (int i = 0; i < array1.length - array.length; i++) {
@@ -53,14 +47,11 @@ public class CDCommand extends Command {
                 }
                 drive.setCurrentDirectory(new Directory("name", path));
             }
-            System.out.println(drive.getCurrentDirectory().getPath());
         }
         else if(parameters.get(0).matches("^[a-zA-Z1-9äüö/]+")){
-            System.out.println("5");
             if(parameters.get(0).split(" ").length != 0){
                 drive.getCurrentDirectory().setPath(drive.getCurrentDirectory().getPath() + "/" + parameters.get(0));
             }
-            System.out.println(drive.getCurrentDirectory().getPath());
         }
     }
 }
